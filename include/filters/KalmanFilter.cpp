42e813c4d86a9f281e3e3bf15aaab4b298977b2a
Elena Kern <elenakern@DN0a22a719.SUNet>
2019-03-17 12:39:57 -0700

    created include folder with header and src files

---
+++ include/filters/KalmanFilter.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "KalmanFilter.h"
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::KalmanFilter(
+	double dt,
+	const Eigen::MatrixXd& A,
+	const Eigen::MatrixXd& C,
+	const Eigen::MatrixXd& Q,
+	const Eigen::MatrixXd& R,
+	const Eigen::MatrixXd& P)
+: A(A), C(C), Q(Q), R(R), P0(P),
+  m(C.rows()), n(A.rows()), dt(dt), initialized(false),
+  I(n, n), x_hat(n), x_hat_new(n)
+
+ {
+ 	I.setIdentity();
+ }
+
+
+ void KalmanFilter::init() {
+ 	x_hat.setZero();
+ 	P = P0; //Try P0 = I
+ 	t0 = 0;
+ 	t = t0;
+ 	initialized = true;
+ }
+
+ void KalmanFilter::init(double t0, const Eigen::VectorXd& x0){
+ 	x_hat = x0;
+ 	P = P0; //Try P0 = I
+ 	this->t0 = t0;
+ 	t = t0;
+ 	initialized = true;
+ }
+
+ void KalmanFilter::update(const Eigen::VectorXd& y){
+
+ 	if(!initialized)
+ 		throw std::runtime_error("Kalman Filter is not initialized!");
+ 	//First phase: Prediction
+ 	x_hat_new = A * x_hat; //assuming no noise
+ 	P = A*P*A.transpose() + Q;
+
+ 	//Second phase: Correction of predicted variables based on Kalman gain
+ 	K = P*C.transpose()*(C*P*C.transpose() + R).inverse(); //Kalman gain
+ 	x_hat_new += K * (y - C*x_hat_new);
+ 	P = (I - K*C)*P;
+ 	x_hat = x_hat_new; 
+
+ 	t+=dt; 
+
+ }
+
+ void KalmanFilter::update(const Eigen::VectorXd& y, double dt, const Eigen::MatrixXd A){
+ 	this->A = A;
+ 	this->dt = dt;
+ 	update(y);
+ }
+
+ Eigen::VectorXd KalmanFilter::state() 
+	{
+		return x_hat;
+	}
+
+double KalmanFilter::time()
+{
+	return t;
+}
---
+++ include/parameter_estimation/LeastSquare.h
@@ -0,0 +1,100 @@
+#ifndef SAI2_COMMON_LEAST_SQUARE_H
+#define SAI2_COMMON_LEAST_SQUARE_H
+
+#include <Eigen/Dense>
+
+class LeastSquare	
+{
+public:
+	/**
+	 * @brief Constructor that takes the link name and the position of where to compute the velocities/accelerations.
+	 * @param robot           A pointer to a Sai2Model object for the robot 
+	 * @param link_name       The name of the link in the robot at which the kinematics are to be computed
+	 * @param pos_in_link     The position in link where the kinematics are to be comuted
+	 */
+//todo: constructor which determines the algorithm?
+	LeastSquare(bool lin);
+
+	//------------------------------------------------
+	// Methods
+	//------------------------------------------------
+	/**
+	* @brief adds measurements to the algorithm
+	* @param force_measurement		force (linear case) or force/torque measurements with respect to force sensor frame
+	* @param accel_local 			linear acceleration with respect to force sensor frame
+	* @param avel_locel 			angular velocity with respect to force sensor frame
+	* @param aaccel_local 			angular acceleration with respect to frame
+	* @param g_local 				gravity with respect to force sensor frame
+	*/
+	void addData(const Eigen::VectorXd& force_measurment, const Eigen::Vector3d& accel_local, const Eigen::Vector3d& avel_local, const Eigen::Vector3d& aaccel_local, const Eigen::Vector3d& g_local);
+
+	/**
+	* @brief updates the inertial parameter vector based on the chosen algorithm
+	* is called by addData function
+	*/
+	void updateData();
+
+	/** 
+	* @brief 	computes data matrix based on 
+ 	*"Improving Force Control Performance by Computational Elimination of Non-Contact Forces/Torques", D. Kubus, T. Kroeger, F. Wahl, ICRA 2008  
+ 	* @param 	A_data Matrix to which the data matrix will be written
+ 	**/
+	void getDataMatrix(Eigen::MatrixXd& A_data);
+
+	/**  
+	* @brief 	computes linear data matrix based on 
+ 	* "Improving Force Control Performance by Computational Elimination of Non-Contact Forces/Torques", D. Kubus, T. Kroeger, F. Wahl, ICRA 2008  
+ 	* @param 	A_data_lin Matrix to which the linear data matrix will be written
+ 	**/
+	void getDataMatrixLin(Eigen::MatrixXd& A_data_lin);
+
+	/* 
+	* @brief returns inertial parameter vector (4x1) in linear case and full inertial parameter vector (10x1) else
+	*		 based on least squares 
+ 	* 		 linear case: mass, mass*coordinates of center of mass
+ 	*		 else: mass, mass*coordinates of center of mass, elements of the inertia matrix
+ 	*/
+	Eigen::VectorXd getInertialParameterVector();
+
+
+	/*
+	* @brief computes the contact force based on the previously estimated inertial parameters and current kinematic variables: 
+	*        force_contact = force_measured - A_data*phi
+	* @param phi 			previously estimated linear inertial parameter
+	* @param force_measured measured force vector in force sensor frame
+	* @param accel_local 	linear acceleration with respect to force sensor frame
+	* @param avel_locel 	angular velocity with respect to force sensor frame
+	* @param aaccel_local 	angular acceleration with respect to frame
+	* @param g_local 		gravity with respect to force sensor frame
+	*
+ 	*/
+	Eigen::Vector3d computeContactForce(const Eigen::Vector3d& force_measured, const Eigen::VectorXd& phi, const Eigen::Vector3d& accel_local, const Eigen::Vector3d& avel_local, const Eigen::Vector3d& aaccel_local, const Eigen::Vector3d& g_local);
+
+	Eigen::VectorXd computeContactForceTorque(const Eigen::VectorXd& force_torque_measured, const Eigen::VectorXd& phi, const Eigen::Vector3d& accel_local, const Eigen::Vector3d& avel_local, const Eigen::Vector3d& aaccel_local, const Eigen::Vector3d& g_local);
+
+
+private:
+
+	Eigen::Vector3d _accel_local; 	//object linear acceleration in sensor frame
+	Eigen::Vector3d _aaccel_local; 	//object angular acceleration in sensor frame
+	Eigen::Vector3d _avel_local;    //object angular velocity in sensor frame
+	Eigen::Vector3d _g_local; 		//gravity vector in sensor frame
+	Eigen::MatrixXd _A_curr;		//current data matrix
+	Eigen::MatrixXd _A; 			//stack of data matrices
+	Eigen::MatrixXd _A_lin_curr; 	//current linear data matrix
+	Eigen::MatrixXd _A_lin; 		//stack of linear data matrices
+	Eigen::VectorXd _ft; 			//current force/torque measurement
+	Eigen::VectorXd _FT; 			//stack of force/torque measurements
+	Eigen::Vector3d _f; 			//current force measurement
+	Eigen::VectorXd _F; 			//stack of force measurements
+	Eigen::VectorXd _phi; 			//estimated inertial parameter vector
+	Eigen::VectorXd _phi_lin; 		//estimated inertial parameter vector
+	Eigen::VectorXd _ft_contact; 	//computed contact force
+	Eigen::Vector3d _f_contact; 	//computed contact force
+	int _n_measurements;
+	bool _linear_case;				//flag for linear case
+};
+
+
+
+#endif //SAI2_COMMON_LEAST_SQUARE_H 
